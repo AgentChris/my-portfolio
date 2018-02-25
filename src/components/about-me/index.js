13b1811858bbd1e04c90eb7aee272b213cdbee93
chris.lego <poputeacristi@gmail.com>
2018-02-25 19:30:08 +0200

    garden

--- src/components/about-me/index.js
+++ src/components/about-me/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Button } from 'reactstrap';
-import './style.css';
 
 const AboutMe = ({}) => (
-  <div className="">
-    <div className="image-me"/>
-    <p>Well nothing much to say....</p>
-    <button type="button" className="btn btn-primary">Primary</button>
-    <Button color="primary">primary</Button>
+  <div className="row align-items-center about-me">
+    <div className="col-sm-12 col-md-4">
+      <div className="d-flex justify-content-center image-me" />
+    </div>
+    <div className="col-sm-12 col-md-8 mt-4 mt-md-0">
+      <div className="font-italic">
+        Embarked on an adventures which supports people in achieving their dreams which consists
+        in either building great apps or helping them become better fullstack software developers.
+        Programmer since 2009 (good old days) I am software developer passionate about building complete and
+        comprehensive application.
+      </div>
+      <div className="mt-4">
+        <div className="d-block d-sm-none">Currently working at:</div>
+        <div className="d-flex flex-wrap">
+          <div className="d-none d-sm-block">Currently working at:&nbsp;</div>
+          <div className="d-flex align-items-center">
+            <div className="qc-icon" />
+            &nbsp;on&nbsp;<span className="ng-icon-ngs" />&nbsp;Nat Geo projects
+          </div>
+        </div>
+        <div className="d-block d-sm-none mt-2">Stacked used:</div>
+        <div className="d-flex justify-content-between stacks mt-sm-2">
+          <div className="d-none d-sm-block">Stacked used:</div>
+          <div className="icon-size python-icon" />
+          <div className="icon-size django-icon" />
+          <div className="icon-size js-icon" />
+          <div className="icon-size typescript-icon" />
+          <div className="icon-size redux-icon" />
+          <div className="icon-size react-icon" />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
