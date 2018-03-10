5aab7ea47552f72f612e888eeaa3c420615afb8f
chris.lego <poputeacristi@gmail.com>
2018-03-10 16:17:29 +0200

    init logic conversation tree

--- src/components/chat/data.js
+++ src/components/chat/data.js
@@ -1,1 +1,31 @@
-const conversation_tree = {};
+const conversation_tree = {
+  scenario_name: "init_brand_new_conversation",
+  nextConversation: {},
+  messages: first_scenario,
+};
+
+const first_scenario = [{
+  text: "Hey there [:wavesign]", hero: false,
+}, {
+  text: "I am Chris, web developer, based in the land of vampires Transilvania (in Cluj) [:spookey emoji]", hero: false
+},];
+
+
+const takeDecision = () => {
+
+};
+
+const get_meesages = (scenario) => {
+  const currentNode = conversation_tree[scenario];
+  return { messages: currentNode.messages, currentConversation: currentNode.nextConversation };
+};
+
+class ChatBoot {
+  private static currentConversation = "init_brand_new_conversation";
+  start = () => {
+    return get_meesages(this.currentConversation);
+  }
+}
+
+const chatBoot = new ChatBoot();
+chatBoot.start();
