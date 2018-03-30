69f7ff5aa6ff2b859835a29981ddbdd2351c79d3
chris.lego <poputeacristi@gmail.com>
2018-03-31 00:03:39 +0300

    update project style

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -2,11 +2,13 @@ import React from 'react';
 import "./style.css";
 
 const Project = ({ id, url, name, description, index }) => (
-  <div className={`col-md-4 col-sm-12 p-0 project-${index}`}>
-    <div className={`project-wrapper c project-${index} p-1`}>
-      <a href={url} className="project-link">{name}</a>
-      <p className="message-description">{description}</p>
-      {/*<div className="project-link">{name}</div>*/}
+  <div className={`col-md-4 col-sm-12 p-0`}>
+    <div className={`project-${index}-background-color`}>
+      <div className={`project-content project-${index} p-1`}>
+        <a href={url} className="project-link">{name}</a>
+        <p className="message-description">{description}</p>
+        {/*<div className="project-link">{name}</div>*/}
+      </div>
     </div>
   </div>
 );
