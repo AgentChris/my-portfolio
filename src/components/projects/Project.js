13d543fe7b84b088184151f780e4605fb45c92b0
chris.lego <poputeacristi@gmail.com>
2018-03-31 12:08:04 +0300

    project hover on mobile

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -3,13 +3,17 @@ import "./style.css";
 
 const Project = ({ id, url, name, description, index }) => (
   <div className={`col-md-4 col-sm-12 p-0`}>
+    {/*<a href={url} className="project-link">*/}
     <div className={`project-${index}-background-color`}>
-      <div className={`project-content project-${index} p-1`}>
-        <a href={url} className="project-link">{name}</a>
-        <p className="message-description">{description}</p>
-        {/*<div className="project-link">{name}</div>*/}
-      </div>
+      <a href={url} className="project-link font-weight-bold">
+        <div className={`project-content project-${index} p-1`}>
+          <span className="project-link">{name}</span>
+          <p className="message-description">{description}</p>
+          {/*<div className="project-link">{name}</div>*/}
+        </div>
+      </a>
     </div>
+    {/*</a>*/}
   </div>
 );
 
