f4540bc562f2c81de91a0de754190eb9c293d073
chris.lego <poputeacristi@gmail.com>
2018-10-15 11:18:54 +0300

    garden

--- src/libs/GA.js
+++ src/libs/GA.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
 export const get_GA = () => {
-  ReactGA.initialize('UA-120608794-1');
+  return ReactGA.initialize('UA-120608794-4');
 };
 
 const GA_OBJECT = get_GA();
